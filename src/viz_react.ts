703dbd375a4e5f8439d31aecc65117c42138e38b
josh <joshua@marinacci.org>
2021-12-17 16:36:35 -0800

    refactor into second file

---
+++ src/viz_react.ts
@@ -0,0 +1,170 @@
+
+export interface NodeElement {
+    type:string
+    props:any
+}
+export type RComp = (props:any) => NodeElement
+export type Lam = ()=> any
+
+type UseStateResult = [any,any]
+class States {
+    private index: number;
+    private states: any[];
+    constructor() {
+        this.index = -1
+        this.states = []
+    }
+    next(lam: Lam):UseStateResult {
+        this.index++
+        let n = this.index
+        let state = this.states[n]
+        if(!state) state = lam()
+        let set_state = (v) => this.states[n] = v
+        return [state,set_state]
+    }
+
+    clone() {
+        let states = new States()
+        states.states = this.states.slice()
+        return states
+    }
+}
+class Effects {
+    private index: number;
+    private effects: any[];
+    constructor() {
+        this.index = -1
+        this.effects = []
+    }
+
+    next(l: Lam):void {
+        this.index++
+        let state = this.effects[this.index]
+        l()
+        return
+    }
+}
+
+class NodeState {
+    private readonly _states:States
+    private readonly _effects: Effects;
+    name: string;
+    child_index: number;
+    children: NodeState[]
+    elem: NodeElement | null;
+    _old: NodeState | null;
+    constructor(old:NodeState|null) {
+        this._old = old
+        this._states = new States()
+        this._effects = new Effects()
+        this.name = "unknown"
+        this.child_index = -1
+        this.children = []
+        this.elem = null
+        if(old) {
+            this._states = old._states.clone()
+        }
+    }
+
+    states():States {
+        return this._states
+    }
+    effects():Effects {
+        return this._effects
+    }
+}
+
+class Result {
+    private _tree: NodeElement;
+    private _stack: NodeState[];
+    private _old: Result;
+
+    constructor(old: Result) {
+        this._old = old
+        this._stack = []
+        if(this._old) {
+            this._stack.push(new NodeState(old._stack[0]))
+        } else {
+            this._stack.push(new NodeState(null))
+        }
+    }
+
+    start_node(fun:RComp): void {
+        // @ts-ignore
+        // l("starting comp",fun.name)
+        let parent = this.current()
+        parent.child_index += 1
+        let current:NodeState = null
+        if(this._old) {
+            current = new NodeState(parent._old.children[parent.child_index])
+        } else {
+            current = new NodeState(null)
+        }
+        current.name = fun.name
+        this._stack.push(current)
+        parent.children.push(current)
+        // l("full path is",this._calc_path())
+    }
+
+    end_node(ret: NodeElement) {
+        // l("ending comp",ret)
+        let cur = this.current()
+        cur.elem = ret
+        //fix parent
+        this._stack.pop()
+        let parent = this.current()
+        // console.log("parent is",parent)
+        // l("parent is",this._stack[this._stack.length-1])
+    }
+
+    current():NodeState {
+        return this._stack[this._stack.length-1]
+    }
+    tree():NodeElement {
+        return this._stack[0].children[0].elem
+    }
+
+    private _calc_path() {
+        let path = ""
+        for(let n of this._stack) {
+            let ns:NodeState = n
+            path += ":"+ns.name +"["+ns.child_index+"]"
+        }
+        return path
+    }
+
+    dump() {
+        console.log('state. tree',this._tree)
+        console.log("state. stack",this._stack)
+        console.log("============")
+    }
+}
+
+let RESULT:Result = null
+export function RT(old:Result|null,fun:RComp|string):Result {
+    RESULT = new Result(old)
+    RE(fun,{})
+    // RESULT.dump()
+    return RESULT
+}
+
+export function RE(fun:RComp|string,props={}) {
+    if(typeof fun === 'string') {
+        return { type:fun, props}
+    }
+    RESULT.start_node(fun as RComp)
+    let ret = (fun as RComp)(props)
+    if(!ret) throw new Error(`function ${fun} returns empty`)
+    RESULT.end_node(ret)
+    return ret
+}
+
+
+
+export function useState(l:Lam):UseStateResult {
+    return RESULT.current().states().next(l)
+}
+export function useEffect(l:Lam):void {
+    return RESULT.current().effects().next(l)
+}
+
